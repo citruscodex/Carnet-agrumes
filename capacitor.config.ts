91e775dcfde191f7169d732d539a5b24d7a3fae0
citruscodex <tristan.peyrotty@gmail.com>
2026-03-29 00:30:42 +0100

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.citruscodex.carnetagrumes',
+  appName: 'Carnet Agrumes',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
