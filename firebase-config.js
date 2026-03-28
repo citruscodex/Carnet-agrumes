dddb32fd6b618e3231f9c6da5661cb859063a89d
citruscodex <tristan.peyrotty@gmail.com>
2026-03-29 00:29:38 +0100

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAevkU7FIBsowYmA1ra5KHSXulmvJS4vik",
+  authDomain: "citruscodex.firebaseapp.com",
+  projectId: "citruscodex",
+  storageBucket: "citruscodex.appspot.com",
+  messagingSenderId: "898996235223",
+  appId: "1:898996235223:web:88e9c8e0586f87343c7992"
+};
